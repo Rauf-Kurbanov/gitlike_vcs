4e27e1ba2f30eb30f591ff41d242135bd192bbed
rauf_kurbanov <kurbanov.re@gmail.com>
2016-06-23 20:51:31 +0300

    added merge and csp conflict handling

--- src/gitlike_vcs/core.clj
+++ src/gitlike_vcs/core.clj
@@ -1,12 +1,18 @@
 (ns gitlike-vcs.core)
 
-; TODO remove from project
+; TODO remove if won't use
 ;(require '[clojure.core.match :refer [match]])
-(require '[clojure.set :refer [intersection]])
+(require '[clojure.set :refer [intersection union]])
+
+; for now let's asume having map <filename file>
+; instead of directory tree
+(def empty-snapshot
+  {:files {}}
+  )
 
 (def empty-commit
   { :snapshot nil
-    :parent nil }
+    :parent-id nil }
   )
 
 (def empty-branch
@@ -25,7 +31,7 @@
   (let [ {bs :branches cs :commits} repo
         curr-branch    (bs :head)
         new-commit     { :snapshot snapshot
-                         :parent  (curr-branch :commit)}
+                         :parent-id  (curr-branch :commit-id)}
         new-id         (count cs)
         updated-branch (assoc curr-branch :commit-id new-id)
         ]
@@ -46,8 +52,121 @@
     )
   )
 
-;(defn merge [repo name])
-;
+(def empty-conflict
+  {
+   :status false
+   :conflicting {}                                             ;
+   }
+  )
+
+(def empty-merge-res
+  {
+   :status nil
+   :conflicts {}
+   :resolver nil
+   }
+  )
+
+(defn succeed-merge
+  [file]
+  { :status true
+    :conflicting {}
+    :gimme-resolver (constantly file)
+   }
+  )
+
+(defn confl-merge
+  [from to]
+  {
+   :status false
+   :conflict {:from from :to to}
+   :gimme-resolver (fn [resolver] (resolver from to))
+   }
+  )
+
+; [lca f1 f2] -> [status [resolver] -> file]
+(defn three-way-merge-files
+  [lca f1 f2]
+  (let []
+    (cond
+      (= lca f1) (succeed-merge f2)
+      (= lca f2) (succeed-merge f1)
+      (= f1 f2) (succeed-merge f1)
+      :else (confl-merge f1 f2)
+      )
+    )
+  )
+
+
+; Let's assume snapshots always have same files
+; [lca from to] -> [status [[resolver] -> commit]]
+(defn three-way-merge
+  [lca from to new-parent-id]
+  (let [ [fl ff ft] (map
+                      (fn [c] (get-in c [:snapshot :files]))
+                      [lca from to])
+        filenames (keys (union lca from to))
+        file-status-map (loop [stats {}, names filenames]
+                     (let [k (first names)
+                           merge-stat (three-way-merge-files (fl k) (ff k) (ft k))
+                           ]
+                       (if (empty? filenames)
+                         stats
+                         (recur (assoc stats name merge-stat)
+                               (rest names)
+                               )
+                         )
+                       )
+                     )
+        new-files (fn [super-resolver]
+                    (into {}
+                          (for [[k v] file-status-map]
+                            [k ((v :gimme-resolver) super-resolver)])
+                          )
+                    )
+        gimme-super-resolver (fn [super-resolver]
+                               {:snapshot {:files (new-files super-resolver)}
+                                :parent-id   new-parent-id
+                                }
+                               )
+        ]
+    {
+     :statusmap file-status-map
+     :gimme-super-resolver gimme-super-resolver
+     }
+    )
+  )
+
+; merge :: [repo branch-name] -> [status ([resolver] -> repo)]
+(defn merge [repo branch-name]
+  ; TODO make distinct function for [from to lca]
+  ; TODO Eliminate code duplicate
+  (let [ {h :head bs :branches cs :commits} repo
+        bh-where (branch-history repo branch-name)
+        bh-curr (branch-history repo h)
+        fromi (last (branch-history repo h))
+        toi (last (branch-history repo branch-name))
+        lci (last-common bh-where bh-curr)
+        [lca from to] (select-keys (repo :commits) [lci fromi toi])
+        merge-status (three-way-merge lca from to toi)
+        ; TODO gimme-resolver factory
+        gimme-super-resolver (fn [super-resolver]
+                               (let [ gimme-res (merge-status :gimme-super-resolver)
+                                     merge-commit (gimme-res super-resolver)
+                                     ]
+                                 (assoc repo :branches (dissoc bs h)
+                                             :head     branch-name
+                                             :commits  (conj cs merge-commit)
+                                             )
+                                 )
+                               )
+        ]
+    {
+     :statusmap merge-status
+     :gimme-super-resolver gimme-super-resolver
+     }
+    )
+  )
 
 (declare branch-history)
 (declare last-common)
@@ -79,10 +198,16 @@
     )
   )
 
+(defn sorted-to-set
+  [vec]
+  (reduce conj #{} vec)
+  )
+
 (defn last-common
+  "Id of last common ancestor"
   [ids1 ids2]
-  (let [ss1 (into (sorted-set) ids1)
-        ss2 (into (sorted-set) ids2)]
+  (let [ss1 (sorted-to-set ids1)
+        ss2 (sorted-to-set ids2)]
     (last (intersection ss1 ss2))
     )
   )
@@ -110,11 +235,11 @@
     )
   )
 
-; TODO unfold
+; TODO reduce
 (defn prev-ids
   [commit-id repo]
   (loop [curr-id commit-id, id-list [commit-id]]
-    (let [parent-id (get-in repo [:commits curr-id :parent])]
+    (let [parent-id (get-in repo [:commits curr-id :parent-id])]
       (if (nil? parent-id)
         id-list
         (recur
@@ -125,4 +250,3 @@
       )
     )
   )
-
